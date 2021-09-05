042e21e9e0d3ca2af544de2931c10dc48a2e3190
agusslam <agus.slamethermawan@gmail.com>
2021-09-05 21:17:34 +0700

    Berhasil membuat nested

--- controller.js
+++ controller.js
@@ -65,14 +65,26 @@ exports.ubahmahasiswa = function (req, res) {
 };
 
 //menghapus data berdasarkan id
-exports.hapusmahasiswa = function (req,res){
+exports.hapusmahasiswa = function (req, res) {
     var id = req.body.id_mahasiswa;
-    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?',[id],
-    function (error, rows, fields) {
-        if (error) {
-            connection.log(error);
-        } else {
-            response.ok("Berhasil menghapus data", res);
-        }
-    });
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                connection.log(error);
+            } else {
+                response.ok("Berhasil menghapus data", res);
+            }
+        });
+};
+
+//menmapilkan group matakuliah
+exports.tampilgroupmatakuliah = function (req, res) {
+    connection.query('SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ',
+        function (error, rows, fields) {
+            if (error) {
+                connection.log(error);
+            } else {
+                response.oknested(rows, res);
+            }
+        });
 };
