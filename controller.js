c049d29a5cbb354a4a0d9f319ac03025e29c382b
agusslam <agus.slamethermawan@gmail.com>
2021-09-05 17:26:38 +0700

    Berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -5,3 +5,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("APlkasi REST Berjalan",res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
